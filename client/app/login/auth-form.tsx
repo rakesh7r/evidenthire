2010ae426fcc4dafa03574967327a3da7685b3ee
Rakesh G <rakeshgandla200@gmail.com>
2026-01-31 07:22:05 +0530

    user auth added

--- client/app/login/auth-form.tsx
+++ client/app/login/auth-form.tsx
@@ -1,11 +1,29 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { createClient } from '@/utils/supabase/client';
+import { AuthChangeEvent, Session } from '@supabase/supabase-js';
 
 export default function AuthForm() {
 	const supabase = createClient();
+	const router = useRouter();
+
+	useEffect(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+			if (event === 'SIGNED_IN') {
+				console.log('Signed In User:', session?.user);
+				router.push('/');
+				router.refresh();
+			}
+		});
+
+		return () => subscription.unsubscribe();
+	}, [router, supabase]);
 
 	return (
 		<Auth
--- client/utils/supabase/middleware.ts
+++ client/utils/supabase/middleware.ts
@@ -40,5 +40,11 @@ export async function updateSession(request: NextRequest) {
 		return NextResponse.redirect(url);
 	}
 
+	if (user && request.nextUrl.pathname.startsWith('/login')) {
+		const url = request.nextUrl.clone();
+		url.pathname = '/';
+		return NextResponse.redirect(url);
+	}
+
 	return supabaseResponse;
 }
